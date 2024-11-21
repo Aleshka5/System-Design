c6d8606a22eb24a9f6af4d1ce181a62072fa4a07
Aleshka5 <fad80223@gmail.com>
2024-11-21 18:42:27 +0300

    fix getting token fro admin

--- src/config.py
+++ src/config.py
@@ -6,9 +6,11 @@ SECRET_KEY = "your-secret-key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+
 class Message(BaseModel):
     body: str
     user_name: str
+
 
 # Модель данных для чатов
 class Chat(BaseModel):
@@ -16,9 +18,11 @@ class Chat(BaseModel):
     login1: str
     login2: str
 
+
 # Модель данных для стены
 class Wall(BaseModel):
     posts: list[str]
+
 
 # Модель данных для пользователя
 class User(BaseModel):
@@ -29,5 +33,13 @@ class User(BaseModel):
     wall: Optional[Wall] = None
     chats: Optional[list[Chat]] = None
 
+
 # Псевдо-база данных пользователей
-client_db = {"admin":User(login="admin",hashed_password="$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",name="Ivan",surname="Ivanov")}
+client_db = {
+    "admin": User(
+        login="admin",
+        hashed_password="2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b",
+        name="Ivan",
+        surname="Ivanov",
+    )
+}
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from hashlib import sha256
 from config import client_db, ACCESS_TOKEN_EXPIRE_MINUTES, Chat, User, Wall, Message
 from jwt_auth import pwd_context, create_access_token, get_current_client
+
 app = FastAPI()
 
 # Маршрут для получения токена
@@ -14,13 +15,19 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     password_check = False
     if form_data.username in client_db:
         password = client_db[form_data.username].hashed_password
-        if form_data.password == password:
+        print(
+            form_data.password,
+            sha256(form_data.password.encode()).hexdigest(),
+            password,
+        )
+        if sha256(form_data.password.encode()).hexdigest() == password:
             password_check = True
 
     if password_check:
         access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         access_token = create_access_token(
-            data={"sub": form_data.username}, expires_delta=access_token_expires)
+            data={"sub": form_data.username}, expires_delta=access_token_expires
+        )
         return {"access_token": access_token, "token_type": "bearer"}
     else:
         raise HTTPException(
@@ -32,14 +39,20 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
 
 # PUT /users/{login}/send_message/{target_login} - Отправка сообщения
 @app.put("/users/{login}/send_message/{target_login}", response_model=Chat)
-def send_message(login: str, target_login: str, body: str, current_user: str = Depends(get_current_client)):
+def send_message(
+    login: str,
+    target_login: str,
+    body: str,
+    current_user: str = Depends(get_current_client),
+):
     if login in client_db.keys() and target_login in client_db.keys():
         # Находим кто общается
         user = client_db[login]
         target = client_db[target_login]
         # Создаём новое сообщение
-        message = Message(body=body, user_name=user.name if getattr(
-            user, "name") else login)
+        message = Message(
+            body=body, user_name=user.name if getattr(user, "name") else login
+        )
 
         # Если у пользователя есть чаты
         if getattr(user, "chats"):
@@ -68,7 +81,9 @@ def send_message(login: str, target_login: str, body: str, current_user: str = D
 
 # GET /users/{user_id}/wall - Получить список сообщений (требует аутентификации)
 @app.get("/users/{login}/chat/{target_login}", response_model=Chat)
-def get_chat(login: str, target_login: str, current_user: str = Depends(get_current_client)):
+def get_chat(
+    login: str, target_login: str, current_user: str = Depends(get_current_client)
+):
     if login in client_db.keys():
         user = client_db[login]
         if getattr(user, "chats"):
@@ -93,7 +108,9 @@ def get_wall(login: str, current_user: str = Depends(get_current_client)):
 
 # POST /users/{login}/create_post - Создать нового поста на стене
 @app.post("/users/{login}/create_post", response_model=Wall)
-def create_post(login: str, post_txt: str, current_user: str = Depends(get_current_client)):
+def create_post(
+    login: str, post_txt: str, current_user: str = Depends(get_current_client)
+):
     if login in client_db.keys():
         user = client_db[login]
         if getattr(user, "wall"):
@@ -107,7 +124,9 @@ def create_post(login: str, post_txt: str, current_user: str = Depends(get_curre
 
 # GET /users/{user_id} - Получить пользователя по имени и фамилии (требует аутентификации)
 @app.get("/users/{name}/{surname}", response_model=User)
-def get_user_by_name(name: str, surname: str, current_user: str = Depends(get_current_client)):
+def get_user_by_name(
+    name: str, surname: str, current_user: str = Depends(get_current_client)
+):
     for login in client_db.keys():
         if name == client_db[login].name and surname == client_db[login].surname:
             return client_db[login]
@@ -130,8 +149,7 @@ def create_user(user: User, current_user: str):
     else:
         # Предполагается, что при регистрации пользователь передаст не хэшированный пароль
         # (По правильному, конечно, надо сразу на стороне клиента пароль хэшировать)
-        user.hashed_password = sha256(
-            user.hashed_password.encode()).hexdigest()
+        user.hashed_password = sha256(user.hashed_password.encode()).hexdigest()
         client_db[user.login] = user
     print(client_db)
     return user
@@ -139,4 +157,5 @@ def create_user(user: User, current_user: str):
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
